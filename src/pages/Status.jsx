899210b9a73c9f2b2bb8bc2ee3ea5b437caad67c
MounikaD13 <mounika.d3945@gmail.com>
2026-01-02 12:55:13 +0530

    enclave

--- src/pages/Status.jsx
+++ src/pages/Status.jsx
@@ -43,7 +43,7 @@ export default function UserEnquiries() {
             overflow: "hidden",
             position: "relative",
             backgroundImage:
-                "url(https://images.unsplash.com/photo-1623742962105-faae8ad99f84?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1607144113358-9d8dd893a647?q=80&w=1063&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+                "url(/images/photo-1623742962105-faae8ad99f84.avif)",
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat"
